2bf2c974a27da5fadac73baff62f5d271ba6ee5b
Martin Andersen <martin.skovgaard.andersen@gmail.com>
2017-02-13 16:01:50 +0100

    Update build configuration
    - Updated config.sh
    - Added library_builders.sh for building optional dependencies

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # cvxopt-wheels
-Wheels for CVXOPT
+
+Automatic wheel building for [CVXOPT](http://cvxopt.org) based on [multibuild](https://github.com/matthew-brett/multibuild).
+
+## License
+
+CVXOPT license: GPL 3
+
+## Status
+
+[![Build Status](https://travis-ci.org/cvxopt/cvxopt-wheels.svg?branch=master)](https://travis-ci.org/cvxopt/cvxopt-wheels)
--- config.sh
+++ config.sh
@@ -1,14 +1,58 @@
 # Define custom utilities
 # Test for OSX with [ -n "$IS_OSX" ]
 
+# Configure which optional extensions to build
+export CVXOPT_BUILD_DSDP=1
+export CVXOPT_BUILD_FFTW=1
+export CVXOPT_BUILD_GLPK=1
+export CVXOPT_BUILD_GSL=1
+TESTS_DIR="$(pwd)/cvxopt/tests"
+
+source library_builders.sh
+
 function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
-    
+
+    # Build dependencies
+    if [ -z "${IS_OSX}" ]; then
+        build_openblas  # defined in multibuild/library_builders.sh
+    fi
+    if [ "${CVXOPT_BUILD_DSDP}" == "1" ]; then build_dsdp; fi
+    if [ "${CVXOPT_BUILD_FFTW}" == "1" ]; then build_fftw; fi
+    if [ "${CVXOPT_BUILD_GLPK}" == "1" ]; then build_glpk; fi
+    if [ "${CVXOPT_BUILD_GSL}" == "1" ]; then build_gsl; fi
+
+    # Download SuiteSparse
+    if [ ! -e suitesparse-stamp ]; then
+        fetch_unpack http://faculty.cse.tamu.edu/davis/SuiteSparse/SuiteSparse-${SUITESPARSE_VERSION}.tar.gz
+        check_sha256sum archives/SuiteSparse-${SUITESPARSE_VERSION}.tar.gz ${SUITESPARSE_SHA256}
+        touch suitesparse-stamp
+    fi
+
+    if [ -z "${IS_OSX}" ]; then
+        export CVXOPT_BLAS_LIB=openblas
+        export CVXOPT_LAPACK_LIB=openblas
+        export CVXOPT_BLAS_LIB_DIR=${BUILD_PREFIX}/lib
+    fi
+    export CVXOPT_GLPK_LIB_DIR=${BUILD_PREFIX}/lib
+    export CVXOPT_GLPK_INC_DIR=${BUILD_PREFIX}/include
+    export CVXOPT_GSL_LIB_DIR=${BUILD_PREFIX}/lib
+    export CVXOPT_GSL_INC_DIR=${BUILD_PREFIX}/include/gsl
+    export CVXOPT_FFTW_LIB_DIR=${BUILD_PREFIX}/lib
+    export CVXOPT_FFTW_INC_DIR=${BUILD_PREFIX}/include
+    export CVXOPT_DSDP_LIB_DIR=${BUILD_PREFIX}/lib
+    export CVXOPT_DSDP_INC_DIR=${BUILD_PREFIX}/include
+    export CVXOPT_SUITESPARSE_SRC_DIR=`pwd`/SuiteSparse
 }
 
 function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
-    python -c 'import sys; import yourpackage; sys.exit(yourpackage.test())'
+    python -c 'from cvxopt import blas,lapack,cholmod,umfpack'
+    if [ "${CVXOPT_BUILD_DSDP}" == "1" ]; then python -c 'from cvxopt import dsdp'; fi
+    if [ "${CVXOPT_BUILD_FFTW}" == "1" ]; then python -c 'from cvxopt import fftw'; fi
+    if [ "${CVXOPT_BUILD_GLPK}" == "1" ]; then python -c 'from cvxopt import glpk'; fi
+    if [ "${CVXOPT_BUILD_GSL}" == "1" ]; then python -c 'from cvxopt import gsl'; fi
+    python -m unittest discover -s ${TESTS_DIR}
 }
