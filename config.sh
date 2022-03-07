cb3f61e5dc96d30ea807eb4c83ae9770175478c9
Martin S. Andersen <martin.skovgaard.andersen@gmail.com>
2022-03-07 23:29:07 +0100

    Updated multibuild [skip appveyor]

--- config.sh
+++ config.sh
@@ -6,7 +6,7 @@ export CVXOPT_BUILD_DSDP=1
 export CVXOPT_BUILD_FFTW=1
 export CVXOPT_BUILD_GLPK=1
 export CVXOPT_BUILD_GSL=1
-export OPENBLAS_VERSION=0.3.19
+export OPENBLAS_VERSION=0.3.10
 TESTS_DIR="$(pwd)/cvxopt/tests"
 
 source library_builders.sh
